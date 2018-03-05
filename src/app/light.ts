21fd37bc151bb2537a1447cb38ee3bbfa1e3d3f1
Cris <crislaney@gmail.com>
2018-03-04 22:20:15 -0800

    Live updater works

--- src/app/light.ts
+++ src/app/light.ts
@@ -6,9 +6,23 @@ export class Light {
     this.rgb = l_rgb;
     this.bri = l_bri;
     this.is_on = l_is_on;
+    this.transitiontime = 0;
   }
+
+  toReqBody(){
+    let reqDict = {};
+    reqDict['rgb'] = this.rgb;
+    reqDict['bri'] = this.bri;
+    reqDict['is_on'] = this.is_on;
+    reqDict['transitiontime'] = this.transitiontime;
+
+    let light_json = JSON.stringify(reqDict);
+    return light_json;
+  }
+
   name: string;
   rgb: Tuple;
   bri: number;
   is_on: boolean;
+  transitiontime: number;
 }
--- src/app/lights/lights.component.ts
+++ src/app/lights/lights.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Light } from '../light';
 import { ActivatedRoute, Router} from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
 @Component({
   selector: 'app-lights',
   templateUrl: './lights.component.html',
@@ -9,25 +11,22 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class LightsComponent implements OnInit {
-  rgb_vals:{}
   bri_val: number;
   current_color:string;
   public lights: Light[];
 
   getAllLights(){
     let get_url: string = "http://127.0.0.1:5000/lights";
-    console.log("hitting url: " + get_url);
-    console.log("Making sure update worked 2");
     this.http.get(get_url, {responseType: 'text'}).subscribe(data=>{
       let data_json = JSON.parse(data);
       for (let key in data_json){
         let value = data_json[key];
         let red = value['rgb_hex'].substr(0, 2);
-        let green = value['rgb_hex'].substr(2, 4);
-        let blue = value['rgb_hex'].substr(4, 6);
+        let green = value['rgb_hex'].substr(2, 2);
+        let blue = value['rgb_hex'].substr(4, 2);
         
-        let rgb_list:[number, number, number] = [parseInt(red, 10), parseInt(green, 10), 
-          parseInt(blue, 10)];
+        let rgb_list:[number, number, number] = [parseInt(red, 16), parseInt(green, 16), 
+          parseInt(blue, 16)];
 
         let bri = value['bri'];
         let is_on = value['on'];
@@ -35,44 +34,55 @@ export class LightsComponent implements OnInit {
         this.lights.push(new Light(key, rgb_list, bri, is_on))
       }
     });
-    let temp: number = 5;
   }
 
-
-  putColorChange(light_name: string, rgb: {}, bri: number){
+  putLightChange(light: Light){
+    let url_name: string = encodeURI(light.name);
     
+    let put_light_url: string = "http://localhost:5000/light/" + url_name;
+    let body = light.toReqBody();
+    this.http.put(put_light_url, body, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+      responseType: 'text' 
+   }).subscribe(data=>{
+      //nconsole.log(data);
+    })
   }
 
   setCurrentStyles(color: string){
     this.current_color = color;
   }
 
-  colorChange(color_sel: string, color_val: number) {
+  colorChange(light: Light, color_sel: string, color_val: number) {
     let val_str = color_val.toString(16);
     if(val_str.length === 1){
       val_str = "0" + val_str;
     }
-    this.rgb_vals[color_sel] = val_str;
-    this.setCurrentStyles("#" + this.rgb_vals['red'] + this.rgb_vals['green'] + this.rgb_vals['blue']);
+
+    let new_rgb: [number, number, number] = light.rgb;
+    if(color_sel === "red"){
+      new_rgb[0] = color_val;
+    }
+    else if(color_sel === "green"){
+      new_rgb[1] = color_val;
+    }
+    else if(color_sel === "blue"){
+      new_rgb[2] = color_val;
+    }
+
+    this.putLightChange(light);
   }
 
-  briChange(val: number) {
-    this.bri_val = val;
+  briChange(light: Light, val: number) {
+    light.bri = val;
+    this.putLightChange(light);
   };
 
   constructor(private http:HttpClient) { 
     console.log("In constructor");
 
-    this.rgb_vals = {
-      'red':0,
-      'green':0,
-      'blue':0
-    }
     this.lights = []
     this.bri_val = 0;
-    this.colorChange('red', 0);
-    this.colorChange('green', 0);
-    this.colorChange('blue', 0);
 
     this.getAllLights();
   }
