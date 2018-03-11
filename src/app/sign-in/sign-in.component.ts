5c77d083e531a70677eef65055d79da6b0764ef6
Cris <crislaney@gmail.com>
2018-03-10 21:10:50 -0800

    Sign in and sign up work with backend

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  signIn(username: string, password: string) {
+
+    let sign_in_url: string = "http://localhost:5000/auth";
+    let req_body = {
+      'username': username,
+      'password': password
+    }
+
+    try {
+      this.http.post(sign_in_url, req_body, {
+        headers: new HttpHeaders().set('Content-Type', 'application/json'),
+        responseType: 'text'
+      }).subscribe(data => {
+        let resp = JSON.parse(data);
+        localStorage.setItem('access_token', resp['access_token']);
+
+      })
+    } catch (e) {
+      console.log("Sign in failed");
+    }
+
+
+  }
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,13 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
+
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signUp(username: string, pass: string, confpass: string) {
+    if (pass !== confpass) {
+      console.log("error");
+      console.log(pass + " != " + confpass);
+    }
+    else {
+      let sign_in_url: string = "http://localhost:5000/api/account";
+      let req_body = {
+        'username': username,
+        'password': pass
+      }
+
+      try {
+        this.http.post(sign_in_url, req_body, {
+          headers: new HttpHeaders().set('Content-Type', 'application/json'),
+          responseType: 'text'
+        }).subscribe(data => {
+          // let resp = JSON.parse(data);
+          console.log(data);
+        })
+      } catch (e) {
+        console.log("Sign in failed");
+      }
+
+
+    }
+  }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
